1d0ef36f8c105deb9307ef043f4e18658629c321
edward <edward.yu0830@gmail.com>
2020-07-03 17:43:26 +0800

    設定色彩編號

--- os/bootpack.c
+++ os/bootpack.c
@@ -1,12 +1,21 @@
 /*PROTOTYPE*/
 void io_hlt(void);
 void write_mem8(int addr,int data);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void init_palette(void);
+void set_palette(int start,int end,unsigned char *rgb);
 
 void HariMain(void){
 	int i;
 	char *p; //address to BYTE[...]
+	
+	init_palette();
+	
 	p = (char *) 0xa0000;
-	for(i = 0 ; i <= 0xaffff ; i++){
+	for(i = 0 ; i <= 0xffff ; i++){
 		//*(p+i) = i & 0x0f;
 		//or in this way
 		p[i] = i & 0x0f;
@@ -15,4 +24,42 @@ void HariMain(void){
 	while(1){
 		io_hlt();
 	}
+}
+
+void init_palette(){
+	static unsigned char table_rgb[16*3] = {
+		0x00, 0x00, 0x00,	/*  0:¶Â*/
+		0xff, 0x00, 0x00,	/*  1:«G¬õ*/
+		0x00, 0xff, 0x00,	/*  2:«Gºñ*/
+		0xff, 0xff, 0x00,	/*  3:«G¶À*/
+		0x00, 0x00, 0xff,	/*  4:«GÂÅ*/
+		0xff, 0x00, 0xff,	/*  5:«Gµµ*/
+		0x00, 0xff, 0xff,	/*  6:«G¤ô*/
+		0xff, 0xff, 0xff,	/*  7:¥Õ*/
+		0xc6, 0xc6, 0xc6,	/*  8:«G¦Ç*/
+		0x84, 0x00, 0x00,	/*  9:·t¬õ*/
+		0x00, 0x84, 0x00,	/* 10:·tºñ*/
+		0x84, 0x84, 0x00,	/* 11:·t¶À*/
+		0x00, 0x00, 0x84,	/* 12:·tÂÅ*/
+		0x84, 0x00, 0x84,	/* 13:·tµµ*/
+		0x00, 0x84, 0x84,	/* 14:·t¤ô*/
+		0x84, 0x84, 0x84	/* 15:·t¦Ç*/
+	};
+	set_palette(0,15,table_rgb);
+	return;
+}
+
+void set_palette(int start,int end,unsigned char *rgb){
+	int i,eflags;
+	eflags = io_load_eflags();
+	io_cli();
+	io_out8(0x03c8, start);
+	for(i = start;i <= end;i++){
+		io_out8(0x03c9,rgb[0]/4);
+		io_out8(0x03c9,rgb[1]/4);
+		io_out8(0x03c9,rgb[2]/4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);
+	return;
 }
--- os/naskfunc.nas
+++ os/naskfunc.nas
@@ -9,7 +9,10 @@
 
 [FILE "naskfunc.nas"]	;original file name
 
-	GLOBAL _io_hlt,_write_mem8	;function name
+	GLOBAL _io_hlt,_io_cli,_io_sti,_io_stihlt
+	GLOBAL _io_in8,_io_in16,_io_in32
+	GLOBAL _io_out8,_io_out16,_io_out32
+	GLOBAL _io_load_eflags,_io_store_eflags 
 
 ;function entry
 
@@ -17,4 +20,50 @@
 
 _io_hlt:				;void io_hlt(void)
 	HLT
+	RET
+_io_cli:				;void io_cli(void)
+	CLI
+	RET
+_io_sti:				;void io_sti(void)
+	STI
+	RET
+_io_stihlt:				;void io_stihlt(void)
+	STI
+	HLT
+	RET
+_io_in8:				;int io_in8(int port)
+	mov edx,[esp+4]		;port
+	mov eax,0
+	in al,dx
+	RET
+_io_in16:				;int io_in16(int port)
+	mov edx,[esp+4]		;port
+	mov eax,0
+	in ax,dx
+	RET
+_io_in32:				;int io_in32(int port)
+	mov edx,[esp+4]		;port
+	mov eax,0
+	in eax,dx
+	RET
+_io_out8:				;void io_out8(int port,int data)
+	mov edx,[esp+4]
+	mov al,[esp+8]
+	out dx,al
+_io_out16:				;void io_out(int port,int data)
+	mov edx,[esp+4]
+	mov ax,[esp+8]
+	out dx,al
+_io_out32:				;void io_out(int port,int data)
+	mov edx,[esp+4]
+	mov eax,[esp+8]
+	out dx,al
+_io_load_eflags:		;int io_load_eflags()
+	PUSHFD
+	POP eax
+	RET
+_io_store_eflags:		;void io_store_eflags(int eflags)
+	mov eax,[esp+4]
+	PUSH eax
+	POPFD
 	RET
