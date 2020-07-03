15aa5b6ce844f75cee83bb7ade1ef35d191385b1
edward <edward.yu0830@gmail.com>
2020-07-03 15:29:13 +0800

    條紋畫面

--- os/bootpack.c
+++ os/bootpack.c
@@ -5,7 +5,7 @@ void write_mem8(int addr,int data);
 void HariMain(void){
 	int i;
 	for(i = 0xa0000 ; i <= 0xaffff ; i++){
-		write_mem8(i,15);//15 means white
+		write_mem8(i,i & 0x0f);//equals to i mod (0f+1)
 	}
 	while(1){
 		io_hlt();
