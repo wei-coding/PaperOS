d38d8e9a7c032f6774365ebf4b6ec35fa2d4218f
edward <edward.yu0830@gmail.com>
2020-07-03 16:04:19 +0800

    用方括號存取

--- os/bootpack.c
+++ os/bootpack.c
@@ -5,10 +5,12 @@ void write_mem8(int addr,int data);
 void HariMain(void){
 	int i;
 	char *p; //address to BYTE[...]
-	for(i = 0xa0000 ; i <= 0xaffff ; i++){
-		p = (char *)i;
-		*p = i & 0x0f;
-		//substitude by upper expr//write_mem8(i,i & 0x0f);//equals to i mod (0f+1)
+	p = (char *) 0xa0000;
+	for(i = 0 ; i <= 0xaffff ; i++){
+		//*(p+i) = i & 0x0f;
+		//or in this way
+		p[i] = i & 0x0f;
+		//equals to i mod (0f+1)
 	}
 	while(1){
 		io_hlt();
